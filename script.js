74c982277c6bdbca403c3e3ab19897fba5f8cf0e
MSebb23 <matteosabbatini23@gmail.com>
2021-04-09 03:58:15 +0200

    Prints X random email
    X is passed by prompt

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 //?  https://flynn.boolean.careers/exercises/api/random/mail
+const email_numbers = parseInt(prompt("Inserisci il numero di email casuali che vuoi ricevere"));
+
 var app_Vue = new Vue (
     {
         el : "#app_Vue",
         data : {
             email : "a",
-            numero_email : null,
             email_list : [],
         },
         mounted :  function() {
             console.log(this.email_list);
             var self = this;
-            for(var i = 0; i != 10; i++) {
+            for(var i = 0; i != email_numbers; i++) {
                 console.log(i);    
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function (answer) {
 
